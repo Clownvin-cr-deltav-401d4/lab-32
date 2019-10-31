621a947c2fafae09cf2dad75db62f28bd7c68ceb
Calvin G Hall <123chall@gmail.com>
2019-10-31 09:45:50 -0500

    Added next/previous

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -8,35 +8,55 @@ import useItemCount from '../../hooks/useItemCount';
 const TodoList = props => {
   const itemCount = useItemCount();
   const [page, setPage] = useState(0);
+  const pageCount = Math.ceil(props.todoList.length / itemCount.count);
   const start = page * itemCount.count;
   const end = Math.min(start + itemCount.count, props.todoList.length);
 
-  const getButtons = () => {
-    let buttons = [];
-    const count = Math.ceil(props.todoList.length / itemCount.count);
+  /*
+  for page buttons.
+  const count = Math.ceil(props.todoList.length / itemCount.count);
     for (let i = 0; i < count; i++) {
       buttons.push((
         <button key={i} onClick={() => setPage(i)} disabled={page === i}>{ i + 1 }</button>
       ));
     }
+  */
+  const getPageButtons = () => {
+    let buttons = [];
+    if (page < pageCount - 1) {
+      buttons.push(<button onClick={() => setPage(page + 1)}>Next</button>);
+    }
+    if (page > 0) {
+      buttons.push(<button onClick={() => setPage(page - 1)}>Previous</button>);
+    }
     return buttons;
+  }
+
+  const getCountButtons = () => {
+    return (
+      <div>
+        Results per page:
+        <button onClick={() => itemCount.setCount(5)} >5</button>
+        <button onClick={() => itemCount.setCount(10)} >10</button>
+        <button onClick={() => itemCount.setCount(15)} >15</button>
+        <button onClick={() => itemCount.setCount(20)} >20</button>
+      </div>
+    )
   }
 
   return (
     <div>
-      Results per page:
-      <button onClick={() => itemCount.setCount(5)} >5</button>
-      <button onClick={() => itemCount.setCount(10)} >10</button>
-      <button onClick={() => itemCount.setCount(15)} >15</button>
-      <button onClick={() => itemCount.setCount(20)} >20</button>
-      <br />
+      {
+        getCountButtons()
+      }
       <ul>
         { props.todoList.slice(start, end).map(item => (
           <TodoItem key={item._id} item={item} toggleComplete={props.toggleComplete} toggleDetails={props.toggleDetails} deleteItem={props.deleteItem} />
         ))}
       </ul>
+      Page {page + 1} of {pageCount + 1}
       {
-        getButtons()
+        getPageButtons()
       }
     </div>
   );
