490848665336e72ee2746fc7929dde7a0fba7f26
Calvin G Hall <123chall@gmail.com>
2019-11-06 19:36:31 -0600

    Added Redux Form

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers } from 'redux';
 
 import listReducer from './todo-list';
 import detailsReducer from './details';
+import formReducer from './form';
 
 const reducer = combineReducers({
   listReducer,
   detailsReducer,
+  formReducer,
 });
 
 export default () => createStore(reducer);
